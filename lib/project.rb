ff4786be6c467c5b83eae528289b33dce1eaa7bf
Roberto Garza <robertg4567@hotmail.com>
2018-08-06 18:39:40 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,11 @@
 class Project
-  attr_accessor :title
+  attr_accessor :title, :backed_projects
   def initialize(title)
     @title = title
+    @backed_projects = []
   end
 
+  def back_project(project)
+    @backed_projects << project
+  end 
 end
